5c67b8fb1f0590ed3a32379ec638504f811b02af
GremiTiger032 <bgremi1@lsu.edu>
2024-09-03 19:53:56 -0500

    FInal results

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
 function setup() {
-    createCanvas(100, 100);
+    createCanvas(500, 500
+    );
     background(150);
-    ellipse(50, 50, 10);
+    fill(0, 0, 0)
+    ellipse(250, 250, 400);
+
+    fill(255, 255, 255)
+    ellipse(250, 250, 250);
+
+    fill(255, 255, 255)
+    ellipse(250, 250, 150);
+
+    fill(0, 0, 0)
+    ellipse(250, 250, 75);
+
+    fill(0, 0, 0)
+    ellipse(70, 56, 75);
+    fill(0, 0, 0)
+    ellipse(440, 56, 75);
+    fill(0, 0, 0)
+    ellipse(70, 450, 75);
+    fill(0, 0, 0)
+    ellipse(440, 450, 75);
+
+    fill(255, 255, 255)
+    ellipse(88, 250, 40);
+    fill(255, 255, 255)
+    ellipse(120, 150, 40);
+    fill(255, 255, 255)
+    ellipse(120, 350, 40);
+    fill(255, 255, 255)
+    ellipse(250, 90, 40);
+    fill(255, 255, 255)
+    ellipse(380, 350, 40);
+    fill(255, 255, 255)
+    ellipse(250, 410, 40);
+    fill(255, 255, 255)
+    ellipse(375, 150, 40);
+    fill(255, 255, 255)
+    ellipse(410, 250, 40);
+   
 }
 
 function draw() {
